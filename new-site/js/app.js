bc561326aa6d49216f7f91006b3e32164f98d215
Sidzinga <sidzingamaseko@gmail.com>
2024-03-05 12:56:29 +0200

    react-table-changes

--- new-site/js/app.js
+++ new-site/js/app.js
@@ -1,4 +1,4 @@
 
-var copyRight = "&copy;" + new Date().getFullYear()
+// var copyRight = "&copy;" + new Date().getFullYear()
 
-document.getElementById("foot-note").innerHTML = copyRight;
+document.getElementById("foot-note").innerHTML = "&copy; " + new Date().getFullYear();
